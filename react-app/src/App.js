665efc8bb5e36d8161c93bbcfb8406dd7869f579
Isaac Tullos <isaac.tullos@gmail.com>
2022-09-05 20:57:18 -0400

    Create React routes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,13 +2,24 @@ import React from 'react'
 import "bootswatch/dist/litera/bootstrap.min.css"
 import './App.css'
 import Button from 'react-bootstrap/Button'
+import { BrowserRouter as Router, Routes, Route   } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Error from './pages/Error'
+
 
 function App() {
   return (
-    <div className="App">
-      <Button>Hello</Button>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/signup' element={<Signup />}/>
+        <Route path='/' element={<Home />}/>
+        <Route path='*' element={<Error />}/>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
---
+++ react-app/src/pages/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div>
+      Error: Page Not found
+    </div>
+  )
+}
+
+export default Error
