876aad2e26e67a65a25a18d614afc79e383b14ce
Isaac Tullos <isaac.tullos@gmail.com>
2022-09-06 18:40:43 -0400

    Add mysql2/sequel and update environment and Rakefile

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,12 @@
-# require 'sequel'
+require 'sequel'
 
-# ENV['RACK_ENV'] ||= "development"
-# DB_FILE = "db/tullos_bank_#{ENV['RACK_ENV']}.sqlite3"
-# puts "database file name: #{DB_FILE}"
-# Sequel::Model.db = Sequel.sqlite DB_FILE
+ENV['RACK_ENV'] ||= "development"
+DB_FILE = "db/pray4tn_#{ENV['RACK_ENV']}.db"
+puts "database file name: #{DB_FILE}"
+Sequel::Model.db = Sequel.mysql2 DB_FILE
 
-# require 'bundler/setup'
-# Bundler.require(:default, ENV['RACK_ENV'])
+require 'bundler/setup'
+Bundler.require(:default, ENV['RACK_ENV'])
 
 require 'rubygems'
 require 'sinatra'
