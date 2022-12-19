99170389499e2aca35fcfcff47e4fbadbe84e701
Isaac Tullos <isaac.tullos@gmail.com>
2022-12-19 16:18:35 -0500

    Create login form

--- react-app/src/pages/Login.jsx
+++ react-app/src/pages/Login.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function Login() {
   return (
-    <div>
-      Login Page
+    <div className="form-body
+                    d-flex
+                    flex-column
+                    justify-content-center
+                    align-items-center">
+      <div className="form-hero
+                      d-flex
+                      justify-content-center
+                      align-items-center">
+        <h1 className='form-hero-text'>Pastor4Life</h1>
+      </div>
+      <div className="form-container">
+        <Form className="login-form">
+          <Form.Group className="mb-3 form" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3 form" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember me" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
