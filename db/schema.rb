7ac1bfed6dfb2ccb7f5ad953562e57e9fc5284dc
ittullos <isaac.tullos@gmail.com>
2024-06-17 15:10:52 -0400

    Add journeys
    This commit adds the Journey model. Journeys are different goals
    that a user can set for themselves. When a goal is set a new
    Commitment is created. For now each Journey is to be completed in
    one year, and the distances of each Journey depicts the distance
    of a TN landmark that has been uploaded by an admin.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_22_224806) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_17_163508) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_22_224806) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_commitments_on_user_id"
+  end
+
+  create_table "journeys", force: :cascade do |t|
+    t.integer "annual_miles", null: false
+    t.integer "monthly_miles", null: false
+    t.integer "weekly_miles", null: false
+    t.string "title", null: false
+    t.index ["title"], name: "index_journeys_on_title", unique: true
   end
 
   create_table "prayers", force: :cascade do |t|
