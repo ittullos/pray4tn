a33a2d6d9697925e4748edb3af118459e10d4dc0
ittullos <isaac.tullos@gmail.com>
2024-06-17 15:59:26 -0400

    Add devotionals
    This commit adds the Devotional model. Devotionals are audio recordings
    that users can listen to. There are no associations with any other models.
    A devotional is simply a unique title, a URL to the audio file, and an
    image URL.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_17_163508) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_17_192115) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_17_163508) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_commitments_on_user_id"
+  end
+
+  create_table "devotionals", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "url", null: false
+    t.string "img_url", null: false
+    t.index ["title"], name: "index_devotionals_on_title", unique: true
   end
 
   create_table "journeys", force: :cascade do |t|
