d6c9c6267028ca92e6ab06f121f70be70dbc2528
Zac Barnard <barnard.zac@gmail.com>
2024-06-19 12:20:06 -0500

    Add migration for Resident position
    Needed to support acts_as_list. Uses a temporary model and an #up and
    a #down method due to differences in data needs in either direction.

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Resident < ActiveRecord::Base
+  belongs_to :user
+
   validates_uniqueness_of :name, case_sensitive: false
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_03_044153) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_07_023028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,7 +55,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_03_044153) do
     t.datetime "loaded_at", default: -> { "CURRENT_TIMESTAMP" }
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
+    t.bigint "user_id"
     t.index ["name"], name: "index_residents_on_name", unique: true
+    t.index ["user_id", "position"], name: "index_residents_on_user_id_and_position", unique: true
+    t.index ["user_id"], name: "index_residents_on_user_id"
   end
 
   create_table "routes", force: :cascade do |t|
