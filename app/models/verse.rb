30977b656750c31b4dbd296c05e53ddf66a5c66f
ittullos <isaac.tullos@gmail.com>
2024-07-18 13:05:49 -0400

    Add verse_of_the_day method to Verse model
    This commit adds a method to the Verse model that returns the verse
    of the day. At the moment I have only been given 100 verses by the
    client. I am handling this by dividing the day of the year by 100 and
    using the remainder to select the verse of the day. This is not ideal
    as it will repeat the verses every 100 days. Eventually when I have all
    366 verses this will need to be changed.

--- app/models/verse.rb
+++ app/models/verse.rb
@@ -3,4 +3,8 @@
 class Verse < ActiveRecord::Base
   validates_presence_of :day, :scripture, :notation, :version
   validates_uniqueness_of :day
+
+  def self.verse_of_the_day
+    find_by(day: (Date.today.yday % 100) + 1)
+  end
 end
