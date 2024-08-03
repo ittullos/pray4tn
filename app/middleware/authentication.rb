628158c40aeda6f6aa9bb33e6c868bbe94fa86bd
Zac Barnard <barnard.zac@gmail.com>
2024-08-03 15:21:48 -0500

    WIP Add testing support for JWTs

--- app/middleware/authentication.rb
+++ app/middleware/authentication.rb
@@ -7,6 +7,14 @@ module Authentication
     end
 
     def call(env)
+      # Get the token from the header
+      # verify the token
+      # grab the user from the decoded token
+      #
+      # token = extract_token(Rack::Request.new(env))
+      # payload = (JsonWebToken.new(token).verify!)
+      # user = User.find_by_sub(payload[:sub])
+
       email = Rack::Request.new(env).fetch_header('HTTP_P4L_EMAIL') { nil }
       user = User.find_by_email(email)
 
@@ -16,6 +24,11 @@ module Authentication
       else
         Rack::Response.new({ data: '', errors: 'Unauthorized' }.to_json, 401, {}).finish
       end
+    end
+
+    private
+
+    def extract_token(request)
     end
   end
 end
--- spec/support/helpers/authentication_spec_helpers.rb
+++ spec/support/helpers/authentication_spec_helpers.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'jwt'
+
 module AuthenticationSpecHelpers
   def authenticated(user = FactoryBot.create(:user))
     headers.merge!(user_token_header(user))
@@ -12,11 +14,16 @@ module AuthenticationSpecHelpers
 
   def user_token_header(user)
     {
-      'HTTP_P4L_EMAIL' => user.email
+      'HTTP_P4L_EMAIL' => user.email # becomes user_token
     }
   end
 
   def unauthorized_response
     { 'data' => '', 'errors' => 'Unauthorized' }
+  end
+
+  def user_token(user)
+    # user factory needs a sub now, but you knew that
+    JWT.encode(user.sub, 'some_jwt_secret', 'HS256')
   end
 end
