74ba363ef2f21e5458d9dc15405cf9b3f2111791
Zac Barnard <barnard.zac@gmail.com>
2024-08-24 16:03:30 -0500

    WIP Create JWT in AuthenticationSpecHelpers
    To simulate the token we'll get from Cognito, the
    AuthenticationSpecHelpers now generate a JWT from an RSA key and export
    the JWK to the MockJWKClient. For now, it creates a new RSA for every
    token, which we need to clean up to avoid bogging the specs down.

--- spec/support/helpers/authentication_spec_helpers.rb
+++ spec/support/helpers/authentication_spec_helpers.rb
@@ -15,7 +15,7 @@ module AuthenticationSpecHelpers
   def user_token_header(user)
     {
       'HTTP_P4L_EMAIL' => user.email, # becomes user_token
-      'HTTP_AUTHORIZATION' => user_token(user)
+      'HTTP_AUTHORIZATION' => "Bearer #{user_token(user)}"
     }
   end
 
@@ -24,45 +24,28 @@ module AuthenticationSpecHelpers
   end
 
   def user_token(user)
-    # user factory needs a sub now, but you knew that
-    # create a key
-    # create a JWK from a key and export it, then save it to the mock client
-    # MockJWKClient.add_key(exported_key)
-    JWT.encode(user.sub, 'some_jwt_secret', 'RS256') # use the key here
-    # like so:
-    # foobar = OpenSSL::PKey::RSA.generate 1024
-    # JWT.encode(user.sub, foobar, 'RS256')
-    #
-    # 'https://github.com/jwt/ruby-jwt/blob/main/README.md#json-web-key-jwk
-    # optional_parameters = { kid: 'my-kid', use: 'sig', alg: 'RS512' } # need to use private: true to use signing_key as below
-    # jwk = JWT::JWK.new(OpenSSL::PKey::RSA.new(2048), optional_parameters)
+    # DOES THIS NEED TO BE PRIVATE??? or is it private by default?
+    # this should be memoized so we don't generate a brand new one for every spec
+    rsa_key = OpenSSL::PKey::RSA.generate 1024
 
-    # # Encoding
-    # payload = { data: 'data' }
-    # token = JWT.encode(payload, jwk.signing_key, jwk[:alg], kid: jwk[:kid])
+    jwk_descriptive_params = {
+      kid: 'my-kid',
+      se: 'sig',
+      alg: 'RS256',
+      issuer: 'cognito-idp.us-east-1.amazonaws.com',
+      # private: true # do I need this or not?
+    }
+    jwk = JWT::JWK.new(rsa_key, jwk_descriptive_params)
 
-    # # JSON Web Key Set for advertising your signing keys
-    # jwks_hash = JWT::JWK::Set.new(jwk).export
-    # # Import a JWK Hash (showing an HMAC example)
-    # jwk = JWT::JWK.new({ kty: 'oct', k: 'my-secret', kid: 'my-kid' })
+    MockJWKClient.add_key(jwk.export(include_private: true))
 
-    # # Import an OpenSSL key
-    # # You can optionally add descriptive parameters to the JWK
-    # desc_params = { kid: 'my-kid', use: 'sig' }
-    # jwk = JWT::JWK.new(OpenSSL::PKey::RSA.new(2048), desc_params)
-
-    # # Export as JWK Hash (public key only by default)
-    # jwk_hash = jwk.export
-    # jwk_hash_with_private_key = jwk.export(include_private: true)
-
-    # # Export as OpenSSL key
-    # public_key = jwk.verify_key
-    # private_key = jwk.signing_key if jwk.private?
-
-    # # You can also import and export entire JSON Web Key Sets
-    # jwks_hash = { keys: [{ kty: 'oct', k: 'my-secret', kid: 'my-kid' }] }
-    # jwks = JWT::JWK::Set.new(jwks_hash)
-    # jwks_hash = jwks.export
+    jwt_payload =
+      {
+        'data' => { 'sub' => user.sub },
+        'iss' => jwk[:issuer],
+        'aud' => 'P4L-API'
+      }
+    JWT.encode(jwt_payload, jwk.signing_key, 'RS256')
   end
 
   class MockJWKClient # subclass the real one
