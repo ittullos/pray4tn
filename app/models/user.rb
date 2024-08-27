7d4000f8a98b373df34aaf77a7038cd8704851fc
Zac Barnard <barnard.zac@gmail.com>
2024-08-27 09:15:13 -0500

    Add sub to User
    The `sub` is the primary key for the User in the Identity Provider
    (IDP), which right now is only Amazon Cognito. This will uniquely
    identify every User and we'll use this to allow access to that User's
    data in our API. This effectively replaces the User email which we
    temporarily used to look up a User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  validates_presence_of :first_name, :last_name, :email
+  validates_presence_of :first_name, :last_name, :email, :sub
   validates_uniqueness_of :email
+  validates_uniqueness_of :sub
 
   has_many :residents, -> { order(position: :asc) }
   attr_accessor :sub
