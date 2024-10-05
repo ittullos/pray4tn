98bb53158a7053f432e25c72e05364b73acbd1f3
ittullos <isaac.tullos@gmail.com>
2024-10-05 15:32:38 -0400

    WIP trying to understand mocks/stubs

--- spec/services/pdf_parser_spec.rb
+++ spec/services/pdf_parser_spec.rb
@@ -5,15 +5,30 @@ require './app/models/user'
 include AuthenticationSpecHelpers
 
 RSpec.describe PdfParser do
+  let(:pdf_file) { Rack::Test::UploadedFile.new('spec/fixtures/sample.pdf', 'application/pdf') }
+  let!(:user) { authenticated }
+  let(:pdf_parser) { double(PdfParser) }
+
   context 'when the PDF file is valid' do
-    let(:pdf_file) { Rack::Test::UploadedFile.new('spec/fixtures/sample.pdf', 'application/pdf') }
-    let!(:user) { authenticated }
-
     it 'processes the uploaded PDF file' do
-      resident_count = Resident.count
-      PdfParser.new({tempfile: pdf_file}, user).load_residents
+      allow(pdf_parser).to receive(:load_residents).and_return(['foobar', 'Tommy Jones', 'information', 'Elon Musk', 'etc.'])
 
-      expect(Resident.count).to eq(resident_count + 23)
+      expect { pdf_parser.load_residents }.to change(Resident, :count).by(2)
+    end
+
+    it 'filters out unnecessary lines'
+
+    it 'creates the users residents'
+
+    it 'skips over duplicate names'
+
+  end
+
+  context 'when the PDF file is not valid' do
+    it 'raises an error' do
+      allow(pdf_parser).to receive(:parse_pdf).and_raise(StandardError.new("Invalid PDF"))
+
+      expect { pdf_parser.parse_pdf }.to raise_error(StandardError, "Invalid PDF")
     end
   end
 end
