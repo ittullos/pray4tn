bf232bee4f9d3cde891680c87e20c9ec44f8de34
ittullos <isaac.tullos@gmail.com>
2025-01-20 12:41:22 -0500

    Makes first and last name optional for user
    This changes the User model to allow for users to not have a first
    or last name. This makes it more simple for new user's who are signing
    up. We can add an info section to the settings screen where they can
    add their first and last name if they want to. We could also add a pop
    up when they first sign up.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  validates_presence_of :first_name, :last_name, :email, :sub
+  validates_presence_of :email, :sub
   validates_uniqueness_of :email
   validates_uniqueness_of :sub
 
