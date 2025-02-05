d63b3efbf10556403c9556d1ba8cf05c82a8953f
ittullos <isaac.tullos@gmail.com>
2025-02-05 16:59:04 -0500

    Adds id_token to authentication spec helpers
    This commit adds the id_token to the authentication spec helpers
    so that the id_token can be required in the authentication middleware
    without causing the tests to fail. The tests have also been changed to
    test a new user as well as a user who's email address has changed. The
    authentication code has also changed to take in only the JWT and not
    the entire idToken object.
    
    There are currently 2 failing tests in the route_spec and I can't seem
    to figure out why. It doesn't look like the headers are coming through
    with the request at all. I will definitely need to fix this before
    merging.

--- app/middleware/authentication.rb
+++ app/middleware/authentication.rb
@@ -9,27 +9,20 @@ module Authentication
     def call(env)
       access_token = extract_access_token(env)
       return unauthorized_response unless access_token
+
       decoded_access_token = JsonWebToken.new(access_token).verify!
       id_token = extract_id_token(env)
+      return unauthorized_response unless id_token
 
-      if id_token
-        id_token_json = JSON.parse(id_token)
-        email = id_token_json.dig('payload', 'email')
-      end
+      decoded_id_token = JsonWebToken.new(id_token).verify!
+      email = decoded_id_token.fetch('email')
 
-      user = User.find_or_create_by(sub: decoded_access_token.fetch('sub')) do |u|
+      user = User.find_or_create_by!(sub: decoded_access_token.fetch('sub')) do |u|
         u.email = email
-        u.save!
       end
 
-      if user
-        if email
-          if user.email != email
-            user.email = email
-            user.save!
-          end
-        end
-
+      if user.present?
+        update_user_email_if_needed(user, email)
         env[:user] = user
         @app.call(env)
       else
@@ -40,6 +33,11 @@ module Authentication
     end
 
     private
+
+    def update_user_email_if_needed(user, email)
+      user.email = email
+      user.save! if user.changed?
+    end
 
     def unauthorized_response
       Rack::Response.new({ data: '', errors: 'Unauthorized' }.to_json, 401, {}).finish
--- spec/middleware/authentication_spec.rb
+++ spec/middleware/authentication_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'Authentication middleware' do
 
   describe 'Cognito' do
     subject { Authentication::Cognito.new(app) }
-    let(:user) { create(:user) }
+    let(:user) { create(:user, email: 'original@example.com') }
     let(:env) { Rack::MockRequest.env_for }
     let(:access_token_double) do
       instance_double(
@@ -21,17 +21,32 @@ RSpec.describe 'Authentication middleware' do
         }
       )
     end
+    let(:id_token_double) do
+      instance_double(
+        JsonWebToken,
+        verify!: {
+          'sub' => user.sub,
+          'iss' => jwk[:issuer],
+          'aud' => 'P4L-API',
+          'email' => 'new@example.com'
+        }
+      )
+    end
 
     before do
       allow(JsonWebToken).to receive(:new).and_call_original
       allow(JsonWebToken).to receive(:new).with('token').and_return(
         access_token_double
       )
+      allow(JsonWebToken).to receive(:new).with('id_token').and_return(
+        id_token_double
+      )
     end
 
     context 'when the request bears a valid token' do
       before do
         env['HTTP_AUTHORIZATION'] = 'Bearer token'
+        env['HTTP_X_ID_TOKEN'] = 'Bearer id_token'
       end
 
       it 'allows access' do
@@ -46,27 +61,24 @@ RSpec.describe 'Authentication middleware' do
         expect(request_env[:user]).to eq(user)
       end
 
-      context 'when there is an ID token' do
-        let(:id_token_double) do
-          instance_double(
-            JsonWebToken,
-            verify!: { "email" => "gandalf@gmail.com" }
-          )
-        end
-
-        before do
-          env['X-ID-TOKEN'] = 'Bearer id_token'
-          allow(JsonWebToken).to receive(:new).with('id_token').and_return(
-            id_token_double
-          )
-        end
-
-        it 'sets the email for the user' do
+      context 'when the user does not exist' do
+        it 'creates a new user with the email from the ID token' do
+          user.destroy
           allow(app).to receive(:call).and_return([200, {}, 'success'])
 
           response = subject.call(env)
           expect(response).to eq([200, {}, 'success'])
-          expect(User.last.email).to include("isaac.tullos")
+          expect(User.last.email).to eq('new@example.com')
+        end
+      end
+
+      context 'when the user already exists' do
+        it 'updates the users email from the ID token' do
+          allow(app).to receive(:call).and_return([200, {}, 'success'])
+
+          response = subject.call(env)
+          expect(response).to eq([200, {}, 'success'])
+          expect(User.last.email).to eq('new@example.com')
         end
       end
     end
