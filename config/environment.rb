adee3a837bf01b9bbf9b9f20fd4062e3cbe04c1c
ittullos <isaac.tullos@gmail.com>
2025-04-09 20:57:36 -0400

    Only load Dotenv in development or test environments

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,10 @@ $LOAD_PATH.unshift(File.expand_path('app', __dir__))
 require 'bundler/setup'
 Bundler.require(:default, ENV['RACK_ENV'])
 
-Dotenv.load
+# Only load Dotenv in development or test environments
+if %w[development test].include?(ENV['RACK_ENV'])
+  require 'dotenv'
+  Dotenv.load
+end
 
 require_relative '../app/app'
